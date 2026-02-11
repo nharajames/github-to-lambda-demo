0c59d0a63150b3518ae654d1173bfcbd0d3669e1
nharajames <nharayakomaj@gmail.com>
2026-02-11 20:08:17 +0200

    update buildspec and lambda runtime to 3.12

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4]}
+    d = {'col1': [1, 2], 'col2': [3, 4]}
     df = pd.DataFrame(data=d)
     print(df)
     print('Done x1.2')
+    return {
+        "statusCode": 200,
+        "message": "Lambda executed successfully",
+        "data": df.to_dict()
+    }
